78fd9e35345ac680d324ae217b7049e7efe41bae
Evan Jacobs <evan@readwritehack.com>
2021-11-06 18:26:21 -0700

    improved layout and dynamically fetching token info

--- src/index.js
+++ src/index.js
@@ -2,6 +2,45 @@ import { Web3ApiClient } from "@web3api/client-js";
 const web3client = new Web3ApiClient();
 const ensUri = "ens/v2.uniswap.web3api.eth";
 
+const tokenList = [
+  {
+    "chainId": "MAINNET",
+    "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    "name": "Ether",
+    "symbol": "ETH",
+  },
+  {
+    "chainId": "MAINNET",
+    "address": "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+    "name": "Aave",
+    "symbol": "AAVE",
+  },
+  {
+    "chainId": "MAINNET",
+    "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "name": "Dai Stablecoin",
+    "symbol": "DAI",
+  },
+  {
+    "chainId": 1,
+    "name": "Uniswap",
+    "address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+    "symbol": "UNI",
+  },
+  {
+    "chainId": "MAINNET",
+    "address": "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
+    "name": "yearn finance",
+    "symbol": "YFI",
+  },
+  {
+    "chainId": "MAINNET",
+    "address": "0xc944e90c64b2c07662a292be6244bdf05cda44a7",
+    "name": "Graph Token",
+    "symbol": "GRT",
+  }
+];
+
 const token0 = {
   chainId: "MAINNET",
   address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
@@ -46,6 +85,91 @@ const inputAmount = {
   token: token1,
   amount: "1000000000000000000"
 }
+
+function helloWorld(value) {
+  console.log('hello world', value);
+}
+
+window.helloWorld = helloWorld;
+
+/////////////////////
+// TOKEN functions //
+/////////////////////
+async function fetchTokenData(token) {
+  console.log('passed in: "', token, '"');
+  console.log('fetched: ', tokenList[token]);
+  const { data, errors } = await web3client.query({
+    uri: ensUri,
+    query: `query {
+      fetchTokenData(
+        chainId: $chainId,
+        address: $address,
+        symbol: $symbol,
+        name: $name
+      )
+    }`,
+    variables: {
+      chainId: tokenList[token].chainId,
+      address: tokenList[token].address,
+      symbol: tokenList[token].symbol,
+      name: tokenList[token].name
+    }
+  });
+
+  if (errors) {
+    throw errors;
+  }
+
+  if (!data) {
+    throw Error("fetchTokenData return undefined");
+  }
+
+  console.log('fetchTokenData: ', data);
+  return data.fetchTokenData;
+}
+window.fetchTokenData = fetchTokenData;
+
+async function fetchTotalSupply(token) {
+  console.log('passed in: "', token, '"');
+  console.log('fetched: ', tokenList[token]);
+  const { data, errors } = await web3client.query({
+    uri: ensUri,
+    query: `query {
+      fetchTotalSupply(
+        token: $token
+      )
+    }`,
+    variables: {
+      token: token
+    }
+  });
+
+  if (errors) {
+    throw errors;
+  }
+
+  if (!data) {
+    throw Error("fetchTotalSupply return undefined");
+  }
+
+  console.log('fetchTotalSupply: ', data);
+  return data;
+}
+window.fetchTotalSupply = fetchTotalSupply;
+
+//let tokenData = await fetchTokenData(token2);
+//let totalSupplyData = await fetchTotalSupply(web3client, tokenData;
+
+// const token_element = document.createElement('div');
+// token_element.innerHTML = "Token data: <pre><code>" + JSON.stringify(tokenData, undefined, 2) + "</code></pre>";
+
+// const total_supply_element = document.createElement('div');
+// token_element.innerHTML = "Total supply: <pre><code>" + JSON.stringify(totalSupplyData, undefined, 2) + "</code></pre>";
+
+// let token_results = document.getElementById('token-api-results');
+// token_results.appendChild(token_element);
+// token_results.appendChild(total_supply_element);
+
 
 
 /////////////////
@@ -107,7 +231,6 @@ async function fetchPairOutputAmount(client, pair, inputAmount) {
   return data;
 }
 
-
 let amount = 0;
 let pairData = {};
 pairData = await fetchPairData(web3client, token0, token1);
@@ -125,6 +248,7 @@ element2.innerHTML = "Token 2: " + token1.currency.name;
 const element3 = document.createElement('div');
 element3.innerHTML = "Pair data: <pre><code>" + JSON.stringify(pairData, undefined, 2) + "</code></pre>";
 
-document.body.appendChild(element1);
-document.body.appendChild(element2);
-document.body.appendChild(element3);
+let pair_results = document.getElementById('pair-api-results');
+pair_results.appendChild(element1);
+pair_results.appendChild(element2);
+pair_results.appendChild(element3);
